14decb0215353d01bb715b5dcfaaba3bff577562
Sobhan-Alizadeh <Sobhan.Alizadeh.Java@gmail.com>
2025-12-27 10:37:49 +0330

    added Member

--- src/Member.java
+++ src/Member.java
@@ -1,8 +1,21 @@
 public class Member extends Person {
-    private String level;
+    private String membership = "Basic";
 
+    public Member(int id, String name, String phone, String membership) {
+        super(id, name, phone);
+        setMembership(membership);
+    }
     public Member(int id, String name, String phone) {
         super(id, name, phone);
+    }
+
+    public void setMembership(String membership) {
+        if (membership != null && (membership.equals("Basic") || membership.equals("Advanced"))) {}
+        this.membership = membership;
+    }
+
+    public String getMembership() {
+        return membership;
     }
 
     @Override
