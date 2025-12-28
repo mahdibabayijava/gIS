be0067779e14cc947a3366863bf0a5a7d6bca1f1
Sobhan-Alizadeh <Sobhan.Alizadeh.Java@gmail.com>
2025-12-28 11:24:07 +0330

    Hello, World!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
-    }
-}
+        JFrame frame = new JFrame("Library Information System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 720);
+        frame.setLocationRelativeTo(null); //اینجوری بعد از خروج از فول اسکرین به گوشه بالا سمت چپ نمیره
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //Fullscreen window
+        frame.setUndecorated(false); //Keeps the title bar
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+    }// End of main
+}// End of class
